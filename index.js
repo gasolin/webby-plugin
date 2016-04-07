a3653e5edb0345bc759d5f0a878fd04079d31383
gasolin <gasolin@gmail.com>
2016-04-07 16:49:05 +0800

    convert jsdoc to // comment

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-/**
- * Description
- *   hubot plugin template writtern in plain Javascript, compatible with webbybot.
- *
- * Commands:
- *   hubot test - return pong
- *
- * Author:
- *   webby team
- */
+//
+// Description
+//   hubot plugin template writtern in plain Javascript, compatible with webbybot.
+//
+// Commands:
+//   hubot test - Reply with pong
+//
+// Author:
+//   webby team
+//
 module.exports = function(robot) {
   robot.respond(/test$/i, function(res) {
     res.send('pong');
